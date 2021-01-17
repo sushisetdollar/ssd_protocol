a71446cbaf2c8f8a2ed8f2942ded3354bae20a0e
sushisetdollar <77560642+sushisetdollar@users.noreply.github.com>
2021-01-16 21:43:19 -0600

    initial

---
+++ README.md
@@ -0,0 +1,38 @@
+# Sushi Set Dollar
+
+- shorthand name: `sushi dollar`
+- full name: `Sushi Set Dollar`
+- symbol: `SSD`
+- decimals: `18`
+- icon:
+
+## contracts
+### mainnet
+- `0x9541e3690aadf057afcd6968a2dc1fa959763df3` **DAO (SSDS)**
+- `0x4a152f0ea704584c3a0fb7f01c0860c1b3a3a14d` **SSD**
+- `0xbb77e5f2948d162c3cfa8adbafefc235873522ae` **Oracle**
+- `0x7315fea7c18a3bac9695348f9e4679805e82b65e` **UniswapV2 USDC:SSD Pair**
+- `0x68e7b9f58c2b898576d35eff2c91f75ece81f9ad` **LP Incentivation Pool**
+
+## audit of forked codebase
+
+is available [here](https://github.com/sushisetdollar/ssd_protocol/blob/master/audit/REP-Dollar-06-11-20.pdf).
+
+## disclaimer
+this project is an experiment - the protocol is audited, and we've put a significant amount of work into testing as well as generally de-risking the design of its core mechanism, however participants should take great caution as bugs resulting in loss of funds are always a possibility.
+
+```
+Copyright 2021 Sushi Set Devs, based on the works of the Empty Set Squad
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use the included code except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ protocol/contracts/Constants.sol
@@ -0,0 +1,152 @@
+/*
+    Copyright 2021 Sushi Set Devs, based on the works of the Empty Set Squad
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "./external/Decimal.sol";
+
+library Constants {
+    /* Chain */
+    uint256 private constant CHAIN_ID = 1; // Mainnet
+
+    /* Bootstrapping */
+    uint256 private constant BOOTSTRAPPING_PERIOD = 168; // 168 epochs / 7 days
+    uint256 private constant BOOTSTRAPPING_PRICE = 154e16; // 1.54 USDC (targeting 4.5% inflation)
+
+    /* Oracle */
+    address private constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    uint256 private constant ORACLE_RESERVE_MINIMUM = 1e9; // 1,000 USDC
+
+    /* Bonding */
+    uint256 private constant INITIAL_STAKE_MULTIPLE = 1e6; // 100 SSD -> 100M SSDS
+
+    /* Epoch */
+    struct EpochStrategy {
+        uint256 offset;
+        uint256 start;
+        uint256 period;
+    }
+
+    uint256 private constant EPOCH_OFFSET = 0;
+    uint256 private constant EPOCH_START = 1610596800;
+    uint256 private constant EPOCH_PERIOD = 3600;
+
+    /* Governance */
+    uint256 private constant GOVERNANCE_PERIOD = 36;
+    uint256 private constant GOVERNANCE_QUORUM = 33e16; // 33%
+    uint256 private constant GOVERNANCE_SUPER_MAJORITY = 66e16; // 66%
+    uint256 private constant GOVERNANCE_EMERGENCY_DELAY = 6; // 6 epochs
+
+    /* DAO */
+    uint256 private constant ADVANCE_INCENTIVE = 60e18; // 60$
+    uint256 private constant DAO_EXIT_LOCKUP_EPOCHS = 36; // 36 epochs fluid
+
+    /* Pool */
+    uint256 private constant POOL_EXIT_LOCKUP_EPOCHS = 12; // 12 epochs fluid
+
+    /* Market */
+    uint256 private constant COUPON_EXPIRATION = 720;
+    uint256 private constant DEBT_RATIO_CAP = 30e16; // 30%
+
+    /* Regulator */
+    uint256 private constant SUPPLY_CHANGE_DIVISOR = 12e18; // 12
+    uint256 private constant SUPPLY_CHANGE_LIMIT = 10e16; // 10%
+    uint256 private constant ORACLE_POOL_RATIO = 40; // 40%
+
+    /**
+     * Getters
+     */
+    function getUsdcAddress() internal pure returns (address) {
+        return USDC;
+    }
+
+    function getOracleReserveMinimum() internal pure returns (uint256) {
+        return ORACLE_RESERVE_MINIMUM;
+    }
+
+    function getEpochStrategy() internal pure returns (EpochStrategy memory) {
+        return EpochStrategy({
+            offset: EPOCH_OFFSET,
+            start: EPOCH_START,
+            period: EPOCH_PERIOD
+        });
+    }
+
+    function getInitialStakeMultiple() internal pure returns (uint256) {
+        return INITIAL_STAKE_MULTIPLE;
+    }
+
+    function getBootstrappingPeriod() internal pure returns (uint256) {
+        return BOOTSTRAPPING_PERIOD;
+    }
+
+    function getBootstrappingPrice() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: BOOTSTRAPPING_PRICE});
+    }
+
+    function getGovernancePeriod() internal pure returns (uint256) {
+        return GOVERNANCE_PERIOD;
+    }
+
+    function getGovernanceQuorum() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_QUORUM});
+    }
+
+    function getGovernanceSuperMajority() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_SUPER_MAJORITY});
+    }
+
+    function getGovernanceEmergencyDelay() internal pure returns (uint256) {
+        return GOVERNANCE_EMERGENCY_DELAY;
+    }
+
+    function getAdvanceIncentive() internal pure returns (uint256) {
+        return ADVANCE_INCENTIVE;
+    }
+
+    function getDAOExitLockupEpochs() internal pure returns (uint256) {
+        return DAO_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getPoolExitLockupEpochs() internal pure returns (uint256) {
+        return POOL_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getCouponExpiration() internal pure returns (uint256) {
+        return COUPON_EXPIRATION;
+    }
+
+    function getDebtRatioCap() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: DEBT_RATIO_CAP});
+    }
+
+    function getSupplyChangeLimit() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_LIMIT});
+    }
+
+    function getSupplyChangeDivisor() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_DIVISOR});
+    }
+
+    function getOraclePoolRatio() internal pure returns (uint256) {
+        return ORACLE_POOL_RATIO;
+    }
+
+    function getChainId() internal pure returns (uint256) {
+        return CHAIN_ID;
+    }
+}
